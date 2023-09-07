7bfd8336ec0e21cd0a29a2087ca8e9e9bfada1c0
Javier Vargas <Javieralejandroortga@gmail.com>
2023-09-07 12:55:26 -0400

    Fix bugs

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -2,6 +2,6 @@ require_relative "./decorator.rb"
 
 class CapitalizeDecorator < Decorator
   def correct_name
-    return @nameable.corret_name.capitalize
+    return @nameable.correct_name.capitalize
   end
 end
--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,9 +2,6 @@ require_relative "./decorator.rb"
 
 class TrimmerDecorator < Decorator
   def correct_name
-    if @nameable.correct_name.length > 10
-      trimmed_name = @nameable.correct_name[0...10]
-    end
-    return trimmed_name
+    return @nameable.correct_name[0...10]
   end
 end
