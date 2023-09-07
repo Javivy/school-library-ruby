e2a48117510115154515f3b5021aed2b787008c5
Javier Vargas <Javieralejandroortga@gmail.com>
2023-09-06 20:55:53 -0400

    Create constructor, inherit from parent and override can_use_services? method

--- teacher.rb
+++ teacher.rb
@@ -1,5 +1,12 @@
 require_relative "/person.rb"
 
 class Teacher < Person
-  
+  def initialize(specialization)
+    super
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    return true
+  end
 end
