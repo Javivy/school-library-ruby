dc568db381ecbdc2f7ff0d91d2f908fa0fe61688
Javier Vargas <Javieralejandroortga@gmail.com>
2023-09-07 20:36:55 -0400

    Create has-many relationship between person and rental

--- person.rb
+++ person.rb
@@ -2,13 +2,14 @@ require_relative 'nameable'
 
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: false)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @id = Random.rand(1..1000)
+    @rentals = []
     super
   end
 
@@ -22,6 +23,11 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.book = self
   end
 
   private
